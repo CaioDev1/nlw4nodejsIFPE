aa08b9a2b2bd3d3b7e61d0266da9a80688458c36
Caio Felipe <imcaiofelipe@gmail.com>
2021-02-27 15:51:01 -0300

    projeto finalizado

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { getConnection } from "typeorm";
+import { app } from "../app";
+import createConnection from "../database";
+
+describe("Surveys", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new survey", async () => {
+    const res = await request(app).post("/surveys").send({
+      title: "Title example",
+      description: "Description Example",
+    });
+
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("id");
+  });
+
+  it("Should be able to get all surveys", async () => {
+    await request(app).post("/surveys").send({
+      title: "Title example2",
+      description: "Description Example2",
+    });
+    const res = await request(app).get("/surveys");
+
+    expect(res.body.length).toBe(2);
+  });
+});
